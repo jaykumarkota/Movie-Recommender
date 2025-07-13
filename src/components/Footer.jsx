1741002c3aeb8a43319e209b033d335248da5738
jaykumar kota <kota.jaykumar@gamil.com>
2025-07-13 21:45:55 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
 
   return (
-    <></>
+    <div className="footer container section">
+      <p className="footer-msg">Crafted By - <span>JK</span></p>
+    </div>
   );
 }
 export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar.jsx';
+import Footer from '../components/Footer.jsx';
 import { MovieContext } from '../contexts/MovieContext.jsx';
 function MainLayout() {
   return (
@@ -8,6 +9,7 @@ function MainLayout() {
       <main className='main-content'>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
   {/* <MovieContext.Provider value={{ name: "Guruvu Garu" }}> */ }
